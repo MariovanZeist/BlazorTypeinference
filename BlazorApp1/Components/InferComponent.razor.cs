1448e5d4f3e751dead907357b7516fa3b316f320
Mario van Zeist <m.vanzeist@home.nl>
2020-09-03 23:38:13 +0200

    Initial commit

---
+++ BlazorApp1/Components/InferComponent.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+
+namespace BlazorApp1.Components
+{
+	partial class InferComponent<T>
+		//where T : IMyInterface
+	{
+		[Parameter] public IEnumerable<T> Items { get; set; }
+	}
+}
---
+++ BlazorApp1/Components/NotInferComponent.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+
+namespace BlazorApp1.Components
+{
+	public partial class NotInferComponent<T>
+		where T : IMyInterface
+	{
+		[Parameter] public IEnumerable<T> Items { get; set; }
+	}
+}
